35d5d04e2726a4b98ae283ff924debf0db038226
chudjakd <chudjakd@gmail.com>
2020-09-08 14:27:23 +0200

    Pridanie anotacie do seller row mappera

--- src/main/java/com/dejvo/Shop/db/mapper/SellerRowMapper.java
+++ src/main/java/com/dejvo/Shop/db/mapper/SellerRowMapper.java
@@ -2,10 +2,11 @@ package com.dejvo.Shop.db.mapper;
 
 import com.dejvo.Shop.model.Seller;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+@Component
 public class SellerRowMapper implements RowMapper<Seller> {
     @Override
     public Seller mapRow(ResultSet resultSet, int i) throws SQLException {
