38afab77f74aefeaa96837ac06e4280a7bdfb11c
chudjakd <chudjakd@gmail.com>
2020-09-08 14:28:03 +0200

    Uprava seller interface kde read seller vracia uz len sellera nie list sellerov

--- src/main/java/com/dejvo/Shop/db/crudservice/SellerInterface.java
+++ src/main/java/com/dejvo/Shop/db/crudservice/SellerInterface.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface SellerInterface {
 
     public void createSeller(Seller seller);
-    public List<Seller> readSellerById(Long id);
+    public Seller readSellerById(Long id);
     public List<Seller> readAllSellers();
     public int updateSeller (Seller seller, Long id);
     public void deleteSeller (Long id);
