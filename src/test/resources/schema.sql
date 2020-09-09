2c81a503092c2890d5a9923ad3880ff46148610e
chudjakd <chudjakd@gmail.com>
2020-09-09 11:18:49 +0200

    Pridanie schemy pre sql h2 databazu a taktiez uprava properties na h2 databazu

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,27 @@
+
+DROP TABLE IF EXISTS customer;
+CREATE TABLE customer (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  email varchar(45) NOT NULL,
+  address varchar(45) NOT NULL
+);
+
+DROP TABLE IF EXISTS seller;
+CREATE TABLE seller (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  email varchar(45) NOT NULL,
+  address varchar(45) NOT NULL
+);
+
+DROP TABLE IF EXISTS product;
+CREATE TABLE product (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  seller_id int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `info` varchar(80) NOT NULL,
+  value double NOT NULL,
+  created_at datetime NOT NULL,
+  count int(11) NOT NULL
+);
