ef2521aa6346e46e8e52e50c3f1166a6dfb2b49b
chudjakd <chudjakd@gmail.com>
2020-09-10 11:02:43 +0200

    Create shopping inteface s jedinou metodou nakupu

---
+++ src/main/java/com/dejvo/Shop/db/crudservice/ShoppingInterface.java
@@ -0,0 +1,10 @@
+package com.dejvo.Shop.db.crudservice;
+
+import com.dejvo.Shop.db.request.BuyProductRequest;
+
+public interface ShoppingInterface {
+
+    public Integer buyProduct(BuyProductRequest buyProductRequest);
+
+
+}
