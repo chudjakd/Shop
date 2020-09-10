6511d0793aa4381ee14316ce17ed3a9c2efc8fe4
chudjakd <chudjakd@gmail.com>
2020-09-10 11:05:07 +0200

    Uprava kde buy product musi vraciat response

--- src/main/java/com/dejvo/Shop/db/crudservice/ShoppingInterface.java
+++ src/main/java/com/dejvo/Shop/db/crudservice/ShoppingInterface.java
@@ -1,10 +1,11 @@
 package com.dejvo.Shop.db.crudservice;
 
 import com.dejvo.Shop.db.request.BuyProductRequest;
+import com.dejvo.Shop.db.response.BuyProductResponse;
 
 public interface ShoppingInterface {
 
-    public Integer buyProduct(BuyProductRequest buyProductRequest);
+    public BuyProductResponse buyProduct(BuyProductRequest buyProductRequest);
 
 
 }
