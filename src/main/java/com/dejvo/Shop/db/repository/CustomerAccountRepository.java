ca1fd77e2699f007cfffe267cc48552bb23ca4f7
chudjakd <chudjakd@gmail.com>
2020-09-10 08:17:11 +0200

    Uprava update money a pridanie metody na get money by customer id

--- src/main/java/com/dejvo/Shop/db/repository/CustomerAccountRepository.java
+++ src/main/java/com/dejvo/Shop/db/repository/CustomerAccountRepository.java
@@ -3,6 +3,7 @@ package com.dejvo.Shop.db.repository;
 import com.dejvo.Shop.db.mapper.CustomerAccountMapper;
 import com.dejvo.Shop.model.CustomerAccount;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -12,16 +13,19 @@ import org.springframework.stereotype.Component;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 @Component
 public class CustomerAccountRepository {
 
-    @Autowired
     JdbcTemplate jdbcTemplate;
-
-    @Autowired
     CustomerAccountMapper customerAccountMapper;
+
+    public CustomerAccountRepository(JdbcTemplate jdbcTemplate, CustomerAccountMapper customerAccountMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.customerAccountMapper = customerAccountMapper;
+    }
 
     public Integer createCustomerAccount(CustomerAccount customerAccount){
         String createQuery="INSERT INTO CUSTOMER_ACCOUNT (CUSTOMER_ID,MONEY) VALUES (?,?)";
@@ -29,7 +33,7 @@ public class CustomerAccountRepository {
         jdbcTemplate.update(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps= connection.prepareStatement(createQuery);
+                PreparedStatement ps= connection.prepareStatement(createQuery, Statement.RETURN_GENERATED_KEYS);
                 ps.setInt(1,customerAccount.getCustomerid());
                 ps.setDouble(2,customerAccount.getMoney());
                 return ps;
@@ -43,7 +47,7 @@ public class CustomerAccountRepository {
         }
     }
 
-    public CustomerAccount getCustomerAccountById(int idofcustomer){
+    public CustomerAccount getCustomerAccountByIdOfCustomer(int idofcustomer){
         try{
             String getCustomerAccountQuery="SELECT * FROM CUSTOMER_ACCOUNT WHERE CUSTOMER_ID="+idofcustomer;
             return jdbcTemplate.queryForObject(getCustomerAccountQuery,customerAccountMapper);
@@ -55,23 +59,40 @@ public class CustomerAccountRepository {
     }
 
     public Integer updateCustomerAccountMoney (double money, int idofcustomer){
-        String updateMoney="UPDATE CUSTOMER_ACCOUNT SET money=? WHERE ID=?";
-        KeyHolder keyHolder= new GeneratedKeyHolder();
-        jdbcTemplate.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps= connection.prepareStatement(updateMoney);
-                ps.setDouble(1,money);
-                ps.setInt(2,idofcustomer);
-                return ps;
-            }
-        },keyHolder);
+        String updateMoney="UPDATE CUSTOMER_ACCOUNT SET money=? WHERE CUSTOMER_ID=?";
+        try {
+            jdbcTemplate.update(new PreparedStatementCreator() {
+                                    @Override
+                                    public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                                        PreparedStatement ps = connection.prepareStatement(updateMoney);
+                                        ps.setDouble(1, money);
+                                        ps.setInt(2, idofcustomer);
+                                        return ps;
+                                    }
+                                }
+            );
+            return 1;
+        }
+        catch (Exception e){
+            return 0;
+        }
 
-        return keyHolder.getKey().intValue();
     }
 
     public List<CustomerAccount> getAllCustomerAccount(){
         String getAllCustomerAccountQuery="SELECT * FROM CUSTOMER_ACCOUNT";
         return jdbcTemplate.query(getAllCustomerAccountQuery,customerAccountMapper);
+    }
+    /*
+    Davat pozor ze metoda vracia -1
+     */
+    public double getMoneyByCustomerId(int customerid){
+        try{
+            String query="SELECT MONEY FROM CUSTOMER_ACCOUNT WHERE CUSTOMER_ID="+customerid;
+            return jdbcTemplate.queryForObject(query,double.class);
+        }catch (DataAccessException e){
+            return -1;
+        }
+
     }
 }
