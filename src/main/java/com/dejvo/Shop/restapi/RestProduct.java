e05c090ead882e286b2162221e2b9d4435660791
chudjakd <chudjakd@gmail.com>
2020-09-11 18:30:37 +0200

    Pridanie metody create more products

--- src/main/java/com/dejvo/Shop/restapi/RestProduct.java
+++ src/main/java/com/dejvo/Shop/restapi/RestProduct.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class RestProduct {
@@ -29,7 +31,7 @@ public class RestProduct {
             return new ResponseEntity<>("Product with that id doesnt exist",HttpStatus.PRECONDITION_FAILED);
         }
     }
-    // TODO: 9. 9. 2020 Nedovolit pridat produkt bez niektoreho parametra
+    // TODO: 9. 9. 2020 @Update(Neviem toto asi nebudem riesit necham to asi tak mozno niekto chce vytvorit product kde nebude count alebo bude cena 0 ze je to free)Nedovolit pridat produkt bez niektoreho parametra
     @PostMapping("/product")
     public ResponseEntity createProduct(@RequestBody Product product){
         Integer key=productInterface.createProduct(product);
@@ -40,7 +42,10 @@ public class RestProduct {
             return new ResponseEntity<>("Hmm something is bad",HttpStatus.PRECONDITION_FAILED);
         }
     }
-
+    @PostMapping("/products")
+    public ResponseEntity createMoreProducts(@RequestBody List<Product> products){
+        return new ResponseEntity<>(null,HttpStatus.OK);
+    }
     // TODO: 9. 9. 2020 Treba zistit na zaklade coho je mozne upravovat niektore parametre osobitne a niektore len v paroch 
     @PatchMapping("product/{id}")
     public ResponseEntity updateProductWithId(@RequestBody UpdateProductRequest request,@PathVariable("id") int id){
