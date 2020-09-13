360bf10785b580a2f09a5f8282170011f1d9ea42
chudjakd <chudjakd@gmail.com>
2020-09-13 09:15:16 +0200

    Pridanie classy pre produkty

---
+++ src/main/java/com/dejvo/Shop/model/enums/ProductCategory.java
@@ -0,0 +1,22 @@
+package com.dejvo.Shop.model.enums;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCategory {
+
+    private List<String> categoryofproducts= new ArrayList<>();
+
+    public ProductCategory() {
+        List<String> categoryofproducts= new ArrayList<>();
+        categoryofproducts.add("Clothes");
+        categoryofproducts.add("Food stuff");
+        categoryofproducts.add("Toys");
+        categoryofproducts.add("Other");
+        this.categoryofproducts = categoryofproducts;
+    }
+
+    public List<String> getCategoryofproducts() {
+        return categoryofproducts;
+    }
+}
