a5870107b09f6762e6bef3ec2da6a50ef5eb17d2
chudjakd <chudjakd@gmail.com>
2020-09-14 10:15:24 +0200

    Pridanie buyproduct by card kde je nakup productu zalozeny na credit karte

--- src/main/java/com/dejvo/Shop/db/crudservice/implementacia/ShoppingImplementacia.java
+++ src/main/java/com/dejvo/Shop/db/crudservice/implementacia/ShoppingImplementacia.java
@@ -1,6 +1,7 @@
 package com.dejvo.Shop.db.crudservice.implementacia;
 
 import com.dejvo.Shop.db.crudservice.*;
+import com.dejvo.Shop.db.request.BuyProductByCardRequest;
 import com.dejvo.Shop.db.request.BuyProductRequest;
 import com.dejvo.Shop.db.response.BuyProductResponse;
 import com.dejvo.Shop.model.BoughtProduct;
@@ -29,7 +30,7 @@ public class ShoppingImplementacia implements ShoppingInterface {
     }
 
     /*
-        Nie je to moc zrozumitelne tak radsej komentik, 1. if nam zistuje  product ktory pride v requeste existuje
+        Nie je to moc zrozumitelne tak radsej komentik, 1. if nam zistuje ci product ktory pride v requeste existuje
         2. if nam zistuje ci dany customer existuje
         3. if nam zistuje ci customer ktory chce nakupit urcity pocet produktov ma dostatocny pocet penazi
         4. if nam zistuje ci je pocet produktov na sklade vacsi alebo rovny tomu ktory prisiel z requestu
@@ -38,9 +39,9 @@ public class ShoppingImplementacia implements ShoppingInterface {
     @Override
     public BuyProductResponse buyProduct(BuyProductRequest buyProductRequest) {
 
-        Integer customerid = buyProductRequest.getCustomerid();
-        Integer productid = buyProductRequest.getProductid();
-        Integer countofbuyingproduct = buyProductRequest.getCount();
+        int customerid = buyProductRequest.getCustomerid();
+        int productid = buyProductRequest.getProductid();
+        int countofbuyingproduct = buyProductRequest.getCount();
 
         // 1 if
         if (productInterface.readProductById(productid) == null) {
@@ -53,7 +54,7 @@ public class ShoppingImplementacia implements ShoppingInterface {
         }
 
         // 3. if
-        Integer countofproductatwarehouse = productInterface.readProductById(productid).getCount();
+        int countofproductatwarehouse = productInterface.readProductById(productid).getCount();
         if (countofproductatwarehouse < countofbuyingproduct) {
             return new BuyProductResponse(false, "Nie je dostatocny pocet produktov ");
         }
@@ -65,14 +66,14 @@ public class ShoppingImplementacia implements ShoppingInterface {
                 , productvalue);
         // 4. if
         if (newMoneyOfCustomer != null) {
-            Integer newCountOfProductAtWarehouse = countofproductatwarehouse - countofbuyingproduct;   //Odpocitanie poctu ktory customer nakupil od celkoveho poctu produktu na sklade
+            int newCountOfProductAtWarehouse = countofproductatwarehouse - countofbuyingproduct;   //Odpocitanie poctu ktory customer nakupil od celkoveho poctu produktu na sklade
             productInterface.updateCountOfProduct(productid, newCountOfProductAtWarehouse);         // Update poctu produktov na sklade kedze doslo k uspesnemu nakupu
             customerAccountInterface.updateMoneyOfCustomerAccount(newMoneyOfCustomer, customerid);   //Update penazi customera ktory uspesne nakupil
             BoughtProduct boughtProductFromDatabase = boughtProductInterface.getBoughtProductByCustomerIdAndProductId(customerid, productid);
 
             // 5. if
             if (boughtProductFromDatabase != null) {
-                Integer newCountOfProductInBoughtProduct = boughtProductFromDatabase.getCount() + countofbuyingproduct; //Ak product uz dany zakaznik niekedy nakupoval pridame uz k existujucemu poctu productu
+                int newCountOfProductInBoughtProduct = boughtProductFromDatabase.getCount() + countofbuyingproduct; //Ak product uz dany zakaznik niekedy nakupoval pridame uz k existujucemu poctu productu
                 boughtProductInterface.updateCountOfBoughtProduct(boughtProductFromDatabase, newCountOfProductInBoughtProduct);
                 return new BuyProductResponse(true, "Uspesne pripocitanie poctu produktov kedze customer uz dany produkt raz nakupoval");
             } else {
@@ -86,4 +87,71 @@ public class ShoppingImplementacia implements ShoppingInterface {
 
         }
     }
+
+
+    // 1 if kontroluje ci je cislo karty v spravnom formate
+    //2 if kontroluje ci je datetime karty v spravnom formate
+    // 3 if kontroluje ci je safecode karty v spravnom formate
+    // 4 if kontroluje ci produkt s danym id existuje
+    // 5 if kontroluje ci customer s danym id existuje
+    //6 if kontroluje ci je dostatocny pocet produktov
+    // 7 if kontroluje ci nahodou uz tento customer nekupoval niekedy product s tymto id ak ano tak to zapise uz k existujucemu poctu
+    @Override
+    public BuyProductResponse buyProductByCard(BuyProductByCardRequest buyProductByCardRequest) {
+        String cardnumber=buyProductByCardRequest.getCardnumber();
+        String datecard=buyProductByCardRequest.getDatecard();
+        String safenumber=buyProductByCardRequest.getSafecode();
+        int productid=buyProductByCardRequest.getProductid();
+        int customerid=buyProductByCardRequest.getCustomerid();
+        int countofbuyingproduct=buyProductByCardRequest.getCountofbuyingproduct();
+
+        String regexforcardnumber="(\\d{4})(\\s)(\\d{4})(\\s)(\\d{4})(\\s)(\\d{4})";
+        String regexfordatecard="(\\d{2})[/](\\d{2})";
+        String regexforsafenumber="(\\d{3})";
+        // 1 if
+        if((!cardnumber.matches(regexforcardnumber))||(cardnumber.length()!=19)){
+            return new BuyProductResponse(false,"Skontrolujte prosim card number");
+        }
+        // 2 if
+        if((!datecard.matches(regexfordatecard))||(datecard.length()!=5)){
+            return new BuyProductResponse(false,"Skontrolujte prosim datecard");
+        }
+
+        // 3 if
+        if((!safenumber.matches(regexforsafenumber))||(safenumber.length()!=3)){
+            return new BuyProductResponse(false,"Skontrolujte prosim safe number");
+        }
+
+        // 4 if
+        if (productInterface.readProductById(productid) == null) {
+            return new BuyProductResponse(false, "Product s ID:" + productid + " neexistuje");
+        }
+
+        // 5 if
+        if (customerAccountInterface.getCustomerByIdOfCustomer(customerid) == null) {
+            return new BuyProductResponse(false, "Customer s ID:" + customerid + " neexistuje");
+        }
+
+        // 6. if
+        int countofproductatwarehouse = productInterface.readProductById(productid).getCount();
+        if (countofproductatwarehouse < countofbuyingproduct) {
+            return new BuyProductResponse(false, "Nie je dostatocny pocet produktov ");
+        }
+
+        int newCountOfProductAtWarehouse = countofproductatwarehouse - countofbuyingproduct;   //Odpocitanie poctu ktory customer nakupil od celkoveho poctu produktu na sklade
+        productInterface.updateCountOfProduct(productid, newCountOfProductAtWarehouse);         // Update poctu produktov na sklade kedze doslo k uspesnemu nakupu
+        BoughtProduct boughtProductFromDatabase = boughtProductInterface.getBoughtProductByCustomerIdAndProductId(customerid, productid);
+
+        // 7. if
+        if (boughtProductFromDatabase != null) {
+            int newCountOfProductInBoughtProduct = boughtProductFromDatabase.getCount() + countofbuyingproduct; //Ak product uz dany zakaznik niekedy nakupoval pridame uz k existujucemu poctu productu
+            boughtProductInterface.updateCountOfBoughtProduct(boughtProductFromDatabase, newCountOfProductInBoughtProduct);
+            return new BuyProductResponse(true, "Uspesne pripocitanie poctu produktov kedze customer uz dany produkt raz nakupoval");
+        } else {
+            BoughtProduct newBoughtOfProduct = new BoughtProduct(customerid, productid, countofbuyingproduct, Timestamp.from(Instant.now()));
+            boughtProductInterface.createBoughtProduct(newBoughtOfProduct);
+            return new BuyProductResponse(true, "Uspesny nakup");
+        }
+
     }
+}
