3097bb04e2c8a089570d425a883b23a3acd986b4
chudjakd <chudjakd@gmail.com>
2020-09-15 19:27:29 +0200

    Pridanie oznacenia @Component aby nahodou mapper nevyhadzoval chybu

--- src/main/java/com/dejvo/Shop/db/mapper/LoginMapper.java
+++ src/main/java/com/dejvo/Shop/db/mapper/LoginMapper.java
@@ -2,10 +2,11 @@ package com.dejvo.Shop.db.mapper;
 
 import com.dejvo.Shop.model.Login;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+@Component
 public class LoginMapper implements RowMapper<Login> {
     @Override
     public Login mapRow(ResultSet resultSet, int i) throws SQLException {
