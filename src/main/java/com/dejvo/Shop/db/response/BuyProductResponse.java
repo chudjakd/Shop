08dce610d15d2eae65102ab947ee2e3656d9ae27
chudjakd <chudjakd@gmail.com>
2020-09-15 19:27:00 +0200

    Pridanie bezparametrickeho konstruktora pretoze vyhadzovalo chyby

--- src/main/java/com/dejvo/Shop/db/response/BuyProductResponse.java
+++ src/main/java/com/dejvo/Shop/db/response/BuyProductResponse.java
@@ -18,6 +18,9 @@ public class BuyProductResponse {
         this.errormessage = errormessage;
     }
 
+    public BuyProductResponse() {
+    }
+
     public boolean isSuccess() {
         return success;
     }
