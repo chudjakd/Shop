46050a2e6164a397bea62e0f16ac065e602c3d06
chudjakd <chudjakd@gmail.com>
2020-09-15 13:35:26 +0200

    Pridanie a otestovanie metody getallsellers with statistic

--- src/main/java/com/dejvo/Shop/db/repository/SellerRepository.java
+++ src/main/java/com/dejvo/Shop/db/repository/SellerRepository.java
@@ -1,21 +1,27 @@
 package com.dejvo.Shop.db.repository;
 
+import com.dejvo.Shop.db.mapper.BoughtProductMapper;
+import com.dejvo.Shop.db.mapper.ProductRowMapper;
 import com.dejvo.Shop.db.mapper.SellerRowMapper;
 import com.dejvo.Shop.db.request.UpdateSellerRequest;
+import com.dejvo.Shop.model.BoughtProduct;
+import com.dejvo.Shop.model.Product;
 import com.dejvo.Shop.model.Seller;
+import com.dejvo.Shop.model.SellerWithStatistic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 @Component
@@ -23,11 +29,15 @@ public class SellerRepository {
 
     SellerRowMapper sellerRowMapper;
     JdbcTemplate jdbcTemplate;
+    BoughtProductMapper boughtProductMapper;
+    ProductRowMapper productRowMapper;
 
     @Autowired
-    public SellerRepository(SellerRowMapper sellerRowMapper, JdbcTemplate jdbcTemplate) {
+    public SellerRepository(SellerRowMapper sellerRowMapper, JdbcTemplate jdbcTemplate, BoughtProductMapper boughtProductMapper, ProductRowMapper productRowMapper) {
         this.sellerRowMapper = sellerRowMapper;
         this.jdbcTemplate = jdbcTemplate;
+        this.boughtProductMapper = boughtProductMapper;
+        this.productRowMapper = productRowMapper;
     }
 
     public Integer createSeller(Seller seller){
@@ -95,6 +105,62 @@ public class SellerRepository {
         catch (DataAccessException e){
             System.out.println("nepodarilo sa");
         }
+    }
+
+    public List<SellerWithStatistic> getMostPopularSeller(){
+        try{
+            String url="SELECT product_id from bought_product";
+            List<Integer> allboughtproduct=jdbcTemplate.query(url, new RowMapper<Integer>() {
+                @Override
+                public Integer mapRow(ResultSet resultSet, int i) throws SQLException {
+                    return resultSet.getInt("product_id");
+                }
+            });
+            String urlproduct="SELECT * from product";
+            List<Product> allproducts=jdbcTemplate.query(urlproduct,productRowMapper);
+            HashMap<Integer,Integer> selleridandcountofsellproducts= new HashMap<>();
+            for(Integer value:allboughtproduct){
+                int productid=value;
+                Product productright=allproducts.stream().filter(product -> product.getId()==productid).findFirst().orElse(null);
+                if(productid==0 || productright==null){
+
+                }else{
+                    int sellerid=productright.getSellerId();
+                    if(selleridandcountofsellproducts.containsKey(sellerid)){
+                        int newvalue=selleridandcountofsellproducts.get(sellerid)+1;
+                        selleridandcountofsellproducts.replace(sellerid,newvalue);
+                    }else{
+                        selleridandcountofsellproducts.put(sellerid,1);
+                    }
+                }
+            }
+            String urlseller="SELECT * FROM SELLER";
+            List<Seller> allsellers=jdbcTemplate.query(urlseller,sellerRowMapper);
+            List<SellerWithStatistic> allsellerswithstatistic= new ArrayList<>();
+            for(Seller seller:allsellers){
+                if(selleridandcountofsellproducts.containsKey(seller.getId())){
+                    SellerWithStatistic sellerWithStatistic= new SellerWithStatistic(seller.getId()
+                            ,seller.getName()
+                            ,seller.getEmail()
+                            ,seller.getAddress()
+                            ,selleridandcountofsellproducts.get(seller.getId())
+                            ,0);
+                    allsellerswithstatistic.add(sellerWithStatistic);
+                }else{
+                    SellerWithStatistic sellerWithStatistic= new SellerWithStatistic(seller.getId()
+                            ,seller.getName()
+                            ,seller.getEmail()
+                            ,seller.getAddress()
+                            ,0
+                            ,0);
+                    allsellerswithstatistic.add(sellerWithStatistic);
+                }
+            }
+
+            allsellerswithstatistic.sort(Comparator.comparing(SellerWithStatistic::getCountoftipsofsellproduct));
+
+            return allsellerswithstatistic;
+        }catch (Exception e){return null;}
     }
 
 }
--- src/test/java/com/dejvo/Shop/db/repository/BoughtProductRepositoryTest.java
+++ src/test/java/com/dejvo/Shop/db/repository/BoughtProductRepositoryTest.java
@@ -1,14 +1,21 @@
 package com.dejvo.Shop.db.repository;
 
 import com.dejvo.Shop.model.BoughtProduct;
+import com.dejvo.Shop.model.Product;
+import com.dejvo.Shop.model.Seller;
+import com.dejvo.Shop.model.SellerWithStatistic;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -17,10 +24,23 @@ public class BoughtProductRepositoryTest {
 
     @Autowired
     BoughtProductRepository boughtProductRepository;
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    SellerRepository sellerRepository;
 
     @Test
     public void testovanieBoughtProductu(){
+        Seller seller= new Seller("Selerko","seleris@seler.sk","Selerova 96");
+        Seller seller2= new Seller("Biac","biac@seler.sk","Biacova 96");
+        sellerRepository.createSeller(seller);
+        sellerRepository.createSeller(seller2);
+        Product product= new Product(1,"Lepidlo","Biele lepidlo lacne", BigDecimal.valueOf(0.55),10, Timestamp.from(Instant.now()),"Toys");
+        Product product2= new Product(2,"Tuska","Biele tuska",BigDecimal.valueOf(0.33),5, Timestamp.from(Instant.now()),"Toys");
         BoughtProduct boughtProduct= new BoughtProduct(1,1,10, Timestamp.from(Instant.now()));
+
+        assertNotNull(productRepository.createProduct(product));
+        assertNotNull(productRepository.createProduct(product2));
 
         assertEquals(1,boughtProductRepository.oldCreateBoughtProduct(boughtProduct));
         assertEquals(1,boughtProductRepository.getAllProductByCustomerId(1).size());
@@ -36,6 +56,13 @@ public class BoughtProductRepositoryTest {
         assertEquals(2,boughtProductRepository.getAllProductByProductId(2).size());
 
         assertEquals(2,boughtProductRepository.getAllProductByCustomerId(1).size());
+
+
+        //Testovanie most popular sellera
+        List<SellerWithStatistic> allsellerswithstatic=sellerRepository.getMostPopularSeller();
+
+        assertEquals(2,allsellerswithstatic.get(1).getId());
+
 
 
     }
